0b7d4eb9d23dfaa3a7fd633e63627068c9fd3d21
Akogwu Emmanuel <akogwu.emmanuel24@gmail.com>
2021-03-31 10:36:21 +0100

    all characters mapped out

--- script.js
+++ script.js
@@ -11,27 +11,16 @@ const characterContainer = document.querySelector('.character-container');
 
 window.addEventListener('load', async () => {
   const data = await getCharacter();
-  console.log(data.results);
 
-  function getDOMContent() {
-    const characterImage = data.results[0].image;
-    const characterName = data.results[0].name;
-    const characterGender = data.results[0].gender;
+  const characterData = data.results;
+  console.log(characterData);
 
-    const characterImage1 = data.results[1].image;
-    const characterName1 = data.results[1].name;
-    const characterGender1 = data.results[1].gender;
-    characterContainer.innerHTML = `
-        <div class="character">
-            <img src=${characterImage} />
-            <h3 class="name">${characterName}</h3>
-            <h4>${characterGender}</h4>
-        </div>
-            <div class="character">
-            <img src=${characterImage1} />
-            <h3 class="name">${characterName1}</h3>
-        <h4>${characterGender1}</h4>
-      `;
-  }
-  getDOMContent();
+  characterData.map((character) => {
+    characterContainer.innerHTML += `
+      <div class="character">
+        <img src=${character.image} />
+        <h3 class="name">${character.name}</h3>
+        <h4>${character.gender}</h4>
+      </div>`;
+  });
 });
